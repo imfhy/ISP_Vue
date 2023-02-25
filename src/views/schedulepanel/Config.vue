05f49be759b9c000a7a7e67257624c54042d534a
FHY <51120966+fuhychn@users.noreply.github.com>
2023-02-25 22:08:11 +0800

    Update Config.vue

--- src/views/schedulepanel/Config.vue
+++ src/views/schedulepanel/Config.vue
@@ -2,7 +2,7 @@
   <div id="main">
     <el-card>
       <div>
-        <el-descriptions class="margin-top" title="排程配置" :column="4" :size="size" :label-style="{'font-weight':'bold'}" border>
+        <el-descriptions class="margin-top" title="排程配置" :column="4" :label-style="{'font-weight':'bold'}" border>
           <template slot="extra">
             <el-tooltip class="item" effect="dark" content="刷新表格" placement="top">
               <el-button
