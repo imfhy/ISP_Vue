3a7ea0531b09b8979b9420eeef6ae100b0e14ae8
FHY <51120966+fuhychn@users.noreply.github.com>
2023-02-26 16:46:55 +0800

    外包每日产能配置

--- src/views/outsource/otherdata/DayCapacityConfig.vue
+++ src/views/outsource/otherdata/DayCapacityConfig.vue
@@ -51,9 +51,9 @@
           @selection-change="handleSelectionChange"
         >
           <el-table-column type="selection" width="55" />
-          <el-table-column prop="pack_line" label="包装线" sortable />
-          <el-table-column prop="holiday_begin" label="放假开始时间" sortable />
-          <el-table-column prop="holiday_end" label="放假结束时间" sortable />
+          <el-table-column prop="component_type" label="组件类型" sortable />
+          <el-table-column prop="date_info" label="日期" sortable />
+          <el-table-column prop="capacity" label="产能" sortable />
           <el-table-column width="110" fixed="right" label="操作">
             <template slot-scope="scope">
               <el-button
@@ -96,18 +96,18 @@
       <el-form ref="$form" :model="model" label-position="left" size="small">
         <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.pack_line" prop="pack_line" label="包装线">
-              <el-input v-model="model.pack_line" placeholder="请输入" clearable />
+            <el-form-item :rules="rules.component_type" prop="component_type" label="包装线">
+              <el-input v-model="model.component_type" placeholder="请输入" clearable />
             </el-form-item>
           </el-col>
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.holiday_begin" prop="holiday_begin" label="放假开始时间">
-              <el-date-picker v-model="model.holiday_begin" placeholder="请选择" value-format="yyyy-MM-dd" :style="{width: '100%'}" />
+            <el-form-item :rules="rules.date_info" prop="date_info" label="放假开始时间">
+              <el-date-picker v-model="model.date_info" placeholder="请选择" value-format="yyyy-MM-dd" :style="{width: '100%'}" />
             </el-form-item>
           </el-col>
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.holiday_end" prop="holiday_end" label="放假结束时间">
-              <el-date-picker v-model="model.holiday_end" placeholder="请选择" value-format="yyyy-MM-dd" :style="{width: '100%'}" />
+            <el-form-item :rules="rules.capacity" prop="capacity" label="放假结束时间">
+              <el-date-picker v-model="model.capacity" placeholder="请选择" value-format="yyyy-MM-dd" :style="{width: '100%'}" />
             </el-form-item>
           </el-col>
         </el-row>
@@ -150,9 +150,9 @@
         :cell-style="setCellColor"
         border
       >
-        <el-table-column prop="pack_line" label="包装线" />
-        <el-table-column prop="holiday_begin" label="放假开始时间" />
-        <el-table-column prop="holiday_end" label="放假结束时间" />
+        <el-table-column prop="component_type" label="组件类型" />
+        <el-table-column prop="date_info" label="日期" />
+        <el-table-column prop="capacity" label="产能" />
       </el-table>
       <el-row>
         <el-col :span="8">
@@ -230,13 +230,13 @@ export default {
       table_data: [], // 表格数据
       tableDataExample: [
         {
-          pack_line: 'CL01',
-          holiday_begin: '2022-10-01',
-          holiday_end: '2022-10-02'
+          component_type: '',
+          date_info: '',
+          capacity: ''
         }, {
-          pack_line: '(必填)',
-          holiday_begin: '(必填)',
-          holiday_end: '(必填)'
+          component_type: '(必填)',
+          date_info: '(必填)',
+          capacity: '(必填)'
         }
       ], // 示例的表格数据
       dialogTitle: '', // 表单dialog标题
@@ -258,29 +258,29 @@ export default {
       forms: ['$form'],
       model: {
         id: '',
-        pack_line: '',
-        holiday_begin: '',
-        holiday_end: ''
+        component_type: '',
+        date_info: '',
+        capacity: ''
       },
       // 修改前的表单内容，用于对比表单前后的变化（应用：关闭前提示修改未保存）
       modelOriginal: {
         id: '',
-        pack_line: '',
-        holiday_begin: '',
-        holiday_end: ''
+        component_type: '',
+        date_info: '',
+        capacity: ''
       },
       rules: {
-        pack_line: [{
+        component_type: [{
           required: true,
           message: '包装线不能为空',
           trigger: 'blur'
         }],
-        holiday_begin: [{
+        date_info: [{
           required: true,
           message: '放假开始时间不能为空',
           trigger: 'blur'
         }],
-        holiday_end: [{
+        capacity: [{
           required: true,
           message: '放假结束时间不能为空',
           trigger: 'blur'
@@ -549,7 +549,7 @@ export default {
       }).then(() => {
         const data = {}
         data['id'] = row.id
-        data['pack_line'] = row.pack_line
+        data['component_type'] = row.component_type
         data['user_name'] = this.name
         HandleDelete(data).then(res => {
           if (res.code === 20000) {
