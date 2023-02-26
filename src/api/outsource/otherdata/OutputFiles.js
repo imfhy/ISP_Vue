632d6df99c98524052f89ba44fd77cd09c230961
FHY <51120966+fuhychn@users.noreply.github.com>
2023-02-26 15:01:33 +0800

    外包新增几张表

---
+++ src/api/outsource/otherdata/OutputFiles.js
@@ -0,0 +1,56 @@
+import request from '../../utils/request'
+// 获取表格数据 分页获取
+export function GetTableData(data) {
+  return request({
+    url: '/config/packholiday/get_table_data/',
+    method: 'post',
+    data
+  })
+}
+// 添加数据
+export function AddData(data) {
+  return request({
+    url: '/config/packholiday/add_data/',
+    method: 'post',
+    data
+  })
+}
+// 修改数据
+export function ModifyData(data) {
+  return request({
+    url: '/config/packholiday/modify_data/',
+    method: 'post',
+    data
+  })
+}
+// 删除勾选的数据
+export function DeleteData(data) {
+  return request({
+    url: '/config/packholiday/delete_data/',
+    method: 'post',
+    data
+  })
+}
+// 删除一条数据
+export function HandleDelete(data) {
+  return request({
+    url: '/config/packholiday/handle_delete/',
+    method: 'post',
+    data
+  })
+}
+// 确认导入数据
+export function ImportData(data) {
+  return request({
+    url: '/config/packholiday/import_data/',
+    method: 'post',
+    data
+  })
+}
+// 确认导出数据
+export function ExportData() {
+  return request({
+    url: '/config/packholiday/export_data/',
+    method: 'get'
+  })
+}
