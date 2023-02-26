94b89068af357ed7203681c92e0101f42ca9569f
FHY <51120966+fuhychn@users.noreply.github.com>
2023-02-26 22:28:15 +0800

    外包输出文件

--- src/views/outsource/otherdata/OutputFiles.vue
+++ src/views/outsource/otherdata/OutputFiles.vue
@@ -51,9 +51,12 @@
           @selection-change="handleSelectionChange"
         >
           <el-table-column type="selection" width="55" />
-          <el-table-column prop="pack_line" label="包装线" sortable />
-          <el-table-column prop="holiday_begin" label="放假开始时间" sortable />
-          <el-table-column prop="holiday_end" label="放假结束时间" sortable />
+          <el-table-column prop="file_category" label="文件类别" sortable />
+          <el-table-column prop="file_name" label="文件名" sortable />
+          <el-table-column prop="file_type" label="文件类型" sortable />
+          <el-table-column prop="file_size" label="文件大小" />
+          <el-table-column prop="file_path" label="文件路径" />
+          <el-table-column prop="create_time" label="创建时间" />
           <el-table-column width="110" fixed="right" label="操作">
             <template slot-scope="scope">
               <el-button
@@ -96,18 +99,18 @@
       <el-form ref="$form" :model="model" label-position="left" size="small">
         <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.pack_line" prop="pack_line" label="包装线">
-              <el-input v-model="model.pack_line" placeholder="请输入" clearable />
+            <el-form-item :rules="rules.file_category" prop="file_category" label="文件类别">
+              <el-input v-model="model.file_category" placeholder="请输入" clearable />
             </el-form-item>
           </el-col>
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.holiday_begin" prop="holiday_begin" label="放假开始时间">
-              <el-date-picker v-model="model.holiday_begin" placeholder="请选择" value-format="yyyy-MM-dd" :style="{width: '100%'}" />
+            <el-form-item :rules="rules.file_name" prop="file_name" label="文件名">
+              <el-date-picker v-model="model.file_name" placeholder="请选择" value-format="yyyy-MM-dd" :style="{width: '100%'}" />
             </el-form-item>
           </el-col>
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.holiday_end" prop="holiday_end" label="放假结束时间">
-              <el-date-picker v-model="model.holiday_end" placeholder="请选择" value-format="yyyy-MM-dd" :style="{width: '100%'}" />
+            <el-form-item :rules="rules.file_type" prop="file_type" label="文件类型">
+              <el-date-picker v-model="model.file_type" placeholder="请选择" value-format="yyyy-MM-dd" :style="{width: '100%'}" />
             </el-form-item>
           </el-col>
         </el-row>
@@ -150,9 +153,12 @@
         :cell-style="setCellColor"
         border
       >
-        <el-table-column prop="pack_line" label="包装线" />
-        <el-table-column prop="holiday_begin" label="放假开始时间" />
-        <el-table-column prop="holiday_end" label="放假结束时间" />
+        <el-table-column prop="file_category" label="文件类别" />
+        <el-table-column prop="file_name" label="文件名" />
+        <el-table-column prop="file_type" label="文件类型" />
+        <el-table-column prop="file_size" label="文件大小" />
+        <el-table-column prop="file_path" label="文件路径" />
+        <el-table-column prop="create_time" label="创建时间" />
       </el-table>
       <el-row>
         <el-col :span="8">
@@ -230,13 +236,19 @@ export default {
       table_data: [], // 表格数据
       tableDataExample: [
         {
-          pack_line: 'CL01',
-          holiday_begin: '2022-10-01',
-          holiday_end: '2022-10-02'
+          file_category: '',
+          file_name: '',
+          file_type: '',
+          file_size: '',
+          file_path: '',
+          create_time: ''
         }, {
-          pack_line: '(必填)',
-          holiday_begin: '(必填)',
-          holiday_end: '(必填)'
+          file_category: '(必填)',
+          file_name: '(必填)',
+          file_type: '(必填)',
+          file_size: '',
+          file_path: '',
+          create_time: ''
         }
       ], // 示例的表格数据
       dialogTitle: '', // 表单dialog标题
@@ -258,31 +270,37 @@ export default {
       forms: ['$form'],
       model: {
         id: '',
-        pack_line: '',
-        holiday_begin: '',
-        holiday_end: ''
+        file_category: '',
+        file_name: '',
+        file_type: '',
+        file_size: '',
+        file_path: '',
+        create_time: ''
       },
       // 修改前的表单内容，用于对比表单前后的变化（应用：关闭前提示修改未保存）
       modelOriginal: {
         id: '',
-        pack_line: '',
-        holiday_begin: '',
-        holiday_end: ''
+        file_category: '',
+        file_name: '',
+        file_type: '',
+        file_size: '',
+        file_path: '',
+        create_time: ''
       },
       rules: {
-        pack_line: [{
+        file_category: [{
           required: true,
-          message: '包装线不能为空',
+          message: '文件类别不能为空',
           trigger: 'blur'
         }],
-        holiday_begin: [{
+        file_name: [{
           required: true,
-          message: '放假开始时间不能为空',
+          message: '文件名不能为空',
           trigger: 'blur'
         }],
-        holiday_end: [{
+        file_type: [{
           required: true,
-          message: '放假结束时间不能为空',
+          message: '文件类型不能为空',
           trigger: 'blur'
         }]
       },
@@ -549,7 +567,7 @@ export default {
       }).then(() => {
         const data = {}
         data['id'] = row.id
-        data['pack_line'] = row.pack_line
+        data['file_category'] = row.file_category
         data['user_name'] = this.name
         HandleDelete(data).then(res => {
           if (res.code === 20000) {
